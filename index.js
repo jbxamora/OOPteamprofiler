b989b792d79ba2ac9efa29b320a96b774b3bd440
jbxamora <jbxamora@icloud.com>
2023-01-30 01:03:07 -0800

    built base function to prompt user what kind of employee they are adding to team. Will continue to add case functions/prompts

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-const Manager = 
-const Engineer = 
-const Intern = 
+const Manager = require("./staff/Manager.js");
+const Engineer = require("./staff/Engineer.js");
+const Intern = require("./staff/Intern.js")
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const Team = [];
+
+function createTeam () {
+    inquirer.prompt([{
+        type: "list",
+        message: "What type of employee are you adding to the team?"
+        name: "addEmployeePrompt",
+        choices: ["Manager", "Engineer", "Intern", "Cancel"]
+    }]).then(function (userInput) {
+        switch(userInput.addEmployeePrompt) {
+            case "Manager":
+                addManager();
+                break;
+            case "Engineer":
+                addEngineer();
+                break;
+            case "Intern":
+                addIntern();
+                break;
+        }
+    })
+}
