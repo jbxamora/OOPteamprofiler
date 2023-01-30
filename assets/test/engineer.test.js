c0192c3be4395a8ebf237cf5c1e53047fa3d4c6b
jbxamora <jbxamora@icloud.com>
2023-01-30 02:52:39 -0800

    created test for engineer class

---
+++ assets/test/engineer.test.js
@@ -0,0 +1,19 @@
+import Engineer from "./assets/staff/Engineer.js";
+
+test("Can create Engineer and verify Github and role", () => {
+  const testGithub = "NoTaReAlGiThuB";
+  const expectedRole = "Engineer";
+  const employeeInstance = new Engineer(
+    "John Doe",
+    2,
+    "fakemail@fakedomain.not",
+    testGithub
+  );
+
+  // Verify Github Username
+  expect(employeeInstance.github).toBe(testGithub);
+  expect(employeeInstance.getGithub()).toBe(testGithub);
+
+  // Verify Role
+  expect(employeeInstance.getRole()).toBe(expectedRole);
+});
